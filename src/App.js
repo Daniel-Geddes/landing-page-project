d82946d670f37b0f060a3a9cf4d016869cf415d3
Daniel-Geddes <64063654+Daniel-Geddes@users.noreply.github.com>
2020-10-07 12:26:22 +0100

    redirect to homepage on load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Pages/Home';
 import Products from './Components/Pages/Products';
@@ -13,7 +13,10 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path = "/" exact component={Home}/>
+        <Route exact path="/">
+        <Redirect to="/home" />
+        </Route>
+        <Route path = "/home" exact component={Home}/>
         <Route path = "/products" exact component={Products} />
         <Route path = "/services" exact component={Services} />
         <Route path = "/sign-up" exact component={SignUp} />
